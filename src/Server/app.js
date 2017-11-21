602828bc36d0f59332957c95c056aaf6014bab39
Zana Yakir-QVKG67 <Yakir.Zana@motorolasolutions.com>
2017-11-21 21:50:35 +0200

    Big refactoring on server side

--- src/Server/app.js
+++ src/Server/app.js
@@ -3,14 +3,14 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var data = require('./bl');
+var bl = require('./bl');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../Client/views'));
 app.use(express.static(path.join(__dirname, '../Client/public')));
 
-require('./routes')(app, data);
-require('./sockets')(io, data);
+require('./routes')(app, bl);
+require('./sockets')(io, bl);
 
 http.listen(80, function() {
     console.log('listening on port 80');
