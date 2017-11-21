f7be15775805285cf3e3ddbf40886cc5b3790533
YakirZana <yaqir11@gmail.com>
2017-11-21 19:24:05 +0200

    fix bugs on Locking system

--- src/Server/index.js
+++ src/Server/index.js
@@ -35,36 +35,46 @@ app.get('/', function(req, res) {
 
 
 io.on('connection', function(socket) {
-   var roomId = socket.handshake.headers.referer.split("/").pop();
-   var room = getRoomById(roomId);
-   socket.join(roomId);
+    var roomId = socket.handshake.headers.referer.split("/").pop();
+    var room = getRoomById(roomId);
+    socket.join(roomId);
 
-   socket.on('ready', function(data) {
-        //updateCurrentRoomState(socket, roomId);
-   });
+    socket.on('init', function () {
+        socket.emit('init', room.userInControl);
+    });
 
-   socket.on('update', function(data) {
-       //getRoomById(roomId).base64 = data;
-       socket.broadcast.to(roomId).emit('update', data);
-   });
+    socket.on('disconnect', function () {
+        if (socket.user == undefined) return;
+        releaseControl(socket.user);
+    });
 
-    socket.on('lockFromClient', function(user) {
-        if(user == "")
+    socket.on('update', function (data) {
+        socket.broadcast.to(roomId).emit('update', data);
+    });
+
+    socket.on('lockFromClient', function (user) {
+        if (user == "")
             user = "לא ידוע";
+        socket.user = user;
+        //TODO: need to move to on connection when we have user before asses
         room.isLocked = true;
         room.userInControl = user;
         socket.broadcast.to(roomId).emit('lockFromServer', user);
     });
 
-    socket.on('releaseFromClient', function(user) {
-        if(user == "")
-            user = "לא ידוע";
+    socket.on('releaseFromClient', function (user) {
+        releaseControl(user);
+    });
+
+    function releaseControl(user) {
+        if(room == undefined || user != room.userInControl) return;
         room.isLocked = false;
         room.userInControl = undefined;
         socket.broadcast.to(roomId).emit('releaseFromServer', user);
-    });
-})
+    }
+});
 
 http.listen(80, function() {
-   console.log('listening on localhost:80');
+    console.log('listening on localhost:80');
 });
+
