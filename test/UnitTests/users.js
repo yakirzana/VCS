b250569f723dd7ec7da993dc23719a3e0b993da7
achiadg <achiadg@gmail.com>
2018-01-14 17:33:07 +0200

    add new tests.

--- test/UnitTests/users.js
+++ test/UnitTests/users.js
@@ -57,8 +57,80 @@ exports.group = {
             test.ok(true);
         }
         test.done();
+    },
 
+    testSaveUserPassword: async function (test) {
+        await bl.users.addUser("teacTest1", "1234", "teac", "her", "Male", "teacher@gm.com", true);
+        var user = await bl.users.getUserByUserName("teacTest1");
+        user.password = "123456";
+        await bl.users.saveUser(user);
+        await sleep(1000);
+        var user1 = await bl.users.getUserByUserName("1234");
+        test.ok(user1.password == "123456");
+        await bl.users.deleteUser("teacTest1");
+        test.done();
+    },
+
+    testSaveUserFirstName: async function (test) {
+        await bl.users.addUser("teacTest1", "1234", "teac", "her", "Male", "teacher@gm.com", true);
+        var user = await bl.users.getUserByUserName("teacTest1");
+        user.firstName = "achiad";
+        await bl.users.saveUser(user);
+        await sleep(1000);
+        var user1 = await bl.users.getUserByUserName("1234");
+        test.ok(user1.firstName == "achiad");
+        await bl.users.deleteUser("teacTest1");
+        test.done();
+    },
+
+    testSaveUserLastName: async function (test) {
+        await bl.users.addUser("teacTest1", "1234", "teac", "her", "Male", "teacher@gm.com", true);
+        var user = await bl.users.getUserByUserName("teacTest1");
+        user.lastName = "Gelerenter";
+        await bl.users.saveUser(user);
+        await sleep(1000);
+        var user1 = await bl.users.getUserByUserName("1234");
+        test.ok(user1.lastName == "Gelerenter");
+        await bl.users.deleteUser("teacTest1");
+        test.done();
+    },
+
+    testSaveUserSex: async function (test) {
+        await bl.users.addUser("teacTest1", "1234", "teac", "her", "Male", "teacher@gm.com", true);
+        var user = await bl.users.getUserByUserName("teacTest1");
+        user.sex = "FeMale";
+        await bl.users.saveUser(user);
+        await sleep(1000);
+        var user1 = await bl.users.getUserByUserName("1234");
+        test.ok(user1.sex == "FeMale");
+        await bl.users.deleteUser("teacTest1");
+        test.done();
+    },
+
+    testSaveUserEmail: async function (test) {
+        await bl.users.addUser("teacTest1", "1234", "teac", "her", "Male", "teacher@gm.com", true);
+        var user = await bl.users.getUserByUserName("teacTest1");
+        user.email = "teacher@gmail.com";
+        await bl.users.saveUser(user);
+        await sleep(1000);
+        var user1 = await bl.users.getUserByUserName("1234");
+        test.ok(user1.email == "teacher@gmail.com");
+        await bl.users.deleteUser("teacTest1");
+        test.done();
+    },
+
+    testSaveUserIsTeacher: async function (test) {
+        await bl.users.addUser("teacTest1", "1234", "teac", "her", "Male", "teacher@gm.com", true);
+        var user = await bl.users.getUserByUserName("teacTest1");
+        user.isTeacher = false;
+        await bl.users.saveUser(user);
+        await sleep(1000);
+        var user1 = await bl.users.getUserByUserName("1234");
+        test.ok(user1.isTeacher == false);
+        await bl.users.deleteUser("teacTest1");
+        test.done();
     }
+
 };
 
 
