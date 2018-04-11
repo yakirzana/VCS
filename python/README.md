50f4cd54a56cefe58162d2b7087bdda0aa97d2bb
YakirZana <yA110517>
2018-04-11 21:43:52 +0300

    update config

--- python/README.md
+++
@@ -1,1 +0,0 @@
-This section contains the sentence tagger and the alerter that use to analyze the chat messages from the students, and alerts the teacher when needed.
--- src/Server/config/index.js
+++ src/Server/config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
     'urlDB': 'mongodb://vcs:yosiIsNum1@vcs-team.tk:27017/vcs',
-    'urlRestSendMessage': 'http://77.138.5.60:5002/text_message',
-    'urlRestDrag': 'http://77.138.5.60:5002/drag_message',
-    'urlRestDS': 'http://77.138.5.60:5002/check_ds_message',
-    'urlRestConnect': 'http://77.138.5.60:5002/user_connect_message',
-    'urlRestDisconnect': 'http://77.138.5.60:5002/user_disconnect_message'
+    'urlRestSendMessage': 'http://132.72.23.230:5002/text_message',
+    'urlRestDrag': 'http://132.72.23.230:5002/drag_message',
+    'urlRestDS': 'http://132.72.23.230:5002/check_ds_message',
+    'urlRestConnect': 'http://132.72.23.230:5002/user_connect_message',
+    'urlRestDisconnect': 'http://132.72.23.230:5002/user_disconnect_message'
 };
