ee7d25f9547bf1b165e85b37aed49ac0e7e0c7f1
Zana Yakir-QVKG67 <Yakir.Zana@motorolasolutions.com>
2018-06-05 18:47:31 +0300

    Update config

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # VCS
 Final project for degree
+When cooperative learning meets technology
 
 ### How to run
 nohup sudo forever src/Server/app.js &
 
 ### How to stop
 sudo forever stop 0
+
+### Config Path:
+src/Server/config/index.js
--- src/Server/config/index.js
+++ src/Server/config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
     'urlDB': 'mongodb://vcs:yosiIsNum1@vcs-team.tk:27017/vcs',
-    'urlRestSendMessage': 'http://132.72.23.230:36635/text_message',
-    'urlRestDrag': 'http://132.72.23.230:36635/drag_message',
-    'urlRestDS': 'http://132.72.23.230:36635/check_ds_message',
-    'urlRestConnect': 'http://132.72.23.230:36635/user_connect_message',
-    'urlRestDisconnect': 'http://132.72.23.230:36635/user_disconnect_message'
+    'urlRestSendMessage': 'http://vcs-team.tk:36635/text_message',
+    'urlRestDrag': 'http://vcs-team.tk:36635/drag_message',
+    'urlRestDS': 'http://vcs-team.tk:36635/check_ds_message',
+    'urlRestConnect': 'http://vcs-team.tk:36635/user_connect_message',
+    'urlRestDisconnect': 'http://vcs-team.tk:36635/user_disconnect_message'
 };
